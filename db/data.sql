04c042993435d3f0becfe4f202dbc18d527f6596
Lenore <libreterra@protonmail.com>
2019-10-23 23:03:52 +0200

    fix db typos

--- db/data.sql
+++ db/data.sql
@@ -12,15 +12,15 @@ CREATE TABLE IF NOT EXISTS stonks (
     shares INTEGER
 );
 
-CREATE TABKE IF NOT EXISTS wallets (
+CREATE TABLE IF NOT EXISTS wallets (
     owner VARCHAR(255) NOT NULL UNIQUE,
     stonk INTEGER,
     amount INTEGER,
-    FOREIGN KEY (owner) REFRENCES users(email),
-    FOREIGN KEY (stonk) REFRENCES stonks(id)
+    FOREIGN KEY (owner) REFERENCES users(email),
+    FOREIGN KEY (stonk) REFERENCES stonks(id)
 );
 
-INSERT INTO stonks (name, description, price) VALUES
+INSERT INTO stonks (name, description, shares) VALUES
 ("TechCo", "Tech company that likes to sell your data.", 100),
 ("dbwebb", "Små dbwebbisar hoppar runt bland löven och investerar", 100),
 ("EvilCo", "EvilCo sells chemical weapons and other questionable goods but hey, they make a lot of money.", 100);

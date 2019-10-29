d254773ee876c250d6af4793941279279019bb72
SpaceLenore <libreterra@protonmail.com>
2019-10-29 22:17:46 +0100

    add trade router

---
+++ routes/trade.js
@@ -0,0 +1,37 @@
+const express       = require('express');
+const trade         = require('../src/trader');
+const router        = express.Router();
+
+router.get("/stonks", async (req, res) => {
+    let stonks = await trade.getStonks();
+
+    if (!stonks.error) {
+        res.status(200).json(stonks.data);
+    } else {
+        res.status(stonks.error).send();
+    }
+});
+
+router.post("/buy", async (req, res) => {
+    req.body.buyer = req.decoded.email;;
+    let buy = await trade.buyStonks(req.body);
+
+    if (!buy.error) {
+        res.status(202).send();
+    } else {
+        res.status(buy.error).send();
+    }
+});
+
+
+router.post("/sell", async (req, res) => {
+    let sell = await trade.sellStonks(res.body);
+
+    if (!sell.error) {
+        res.status(202).send();
+    } else {
+        res.status(sell.error).send();
+    }
+});
+
+module.exports = router;
---
+++ src/trader.js
@@ -0,0 +1,70 @@
+const handler = require('../db/handler');
+
+const getStonks = () => {
+    let response = {
+        error: false,
+        data: null
+    }
+    return handler.fetchAll("SELECT * FROM stonks", [])
+    .then((res) => {
+        response.data = res;
+        return response;
+    })
+    .catch((err) => {
+        response.error = 500;
+        return response;
+    });
+}
+
+const buyStonks = async (buying) => {
+    let response = {
+        error: false
+    };
+
+    if (buying === undefined) {
+        response.error = 400;
+        return response;
+    }
+    if (
+        !buying.id ||
+        !buying.price ||
+        !buying.amount ||
+        !buying.buyer
+    ) {
+        response.error = 400;
+        return response;
+    }
+
+    let amountLeft, companyName = await handler.fetchOne("SELECT name, shares FROM stonks WHERE id = ?", [buying.id])
+    .then((res) => {
+        let name = res.name.toLowerCase().replace(/\W/g, '');
+        return res.shares, name;
+    })
+    .catch((err) => {
+        return 0;
+    })
+
+    if (buying.amount > amountLeft) {
+        response.error = 409;
+        return response;
+    }
+
+    let diffAmount = amountLeft - buying.amount;
+
+    try {
+        await handler.exec("UPDATE stonks SET shares=? WHERE id = ?", [diffAmount, buying.id]);
+        await handler.exec("UPDATE users SET " + companyName + "= " + companyName + "+? WHERE email = ?;", [buying.amount, buying.buyer]);
+        await handler.exec("UPDATE users SET funds = funds - ? where email = ?", [buying.price, buying.buyer]);
+        return response;
+    } catch (e) {
+        response.error = 500;
+        return response;
+    }
+
+    return response;
+}
+
+module.exports = {
+    getStonks,
+    buyStonks
+}

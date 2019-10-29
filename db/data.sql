4c2e3f7dfab70565bce58e31a276b34e229f4294
SpaceLenore <libreterra@protonmail.com>
2019-10-29 22:11:54 +0100

    simplified database

--- db/data.sql
+++ db/data.sql
@@ -2,7 +2,10 @@ CREATE TABLE IF NOT EXISTS users (
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL PRIMARY KEY,
     password VARCHAR(64) NOT NULL,
-    funds FLOAT DEFAULT 100
+    funds FLOAT DEFAULT 100,
+    techco INT DEFAULT 0,
+    dbwebb INT DEFAULT 0,
+    evilco INT DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS stonks (
@@ -10,14 +13,6 @@ CREATE TABLE IF NOT EXISTS stonks (
     name VARCHAR(128) NOT NULL,
     description TEXT,
     shares INTEGER
-);
-
-CREATE TABLE IF NOT EXISTS wallets (
-    owner VARCHAR(255) NOT NULL UNIQUE,
-    stonk INTEGER,
-    amount INTEGER,
-    FOREIGN KEY (owner) REFERENCES users(email),
-    FOREIGN KEY (stonk) REFERENCES stonks(id)
 );
 
 INSERT INTO stonks (name, description, shares) VALUES
--- routes/account.js
+++ routes/account.js
@@ -3,7 +3,7 @@ const handler       = require('../db/handler');
 const router        = express.Router();
 
 router.get("/wallets", async (req, res, next) => {
-    let wallets = await handler.fetchAll("SELECT * FROM wallets WHERE owner = ?", [req.decoded.email]);
+    let wallets = await handler.fetchAll("SELECT techco, dbwebb, evilco FROM users WHERE email = ?", [req.decoded.email]);
     res.status(200).json(wallets);
 });
 
